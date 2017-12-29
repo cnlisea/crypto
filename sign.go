0fb5a58e864d161663f718082f9ac9a438a03af8
lisea <lihai@jubao56.com>
2017-12-29 09:50:20 +0800

    add sign file

---
+++ sign.go
@@ -0,0 +1,67 @@
+package crypto
+
+import (
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+)
+
+//SignWithRSA sign given encrypted data with RSA algorithm
+func SignRSA(raw []byte, algorithm crypto.Hash, privateKey string) []byte {
+	if raw == nil {
+		return nil
+	}
+	privateKey := []byte(privateKey)
+
+	block, _ := pem.Decode(privateKey)
+	if block == nil {
+		return nil
+	}
+	privInterface, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+	if err != nil {
+		return nil
+	}
+	priv := privInterface.(*rsa.PrivateKey)
+	var data []byte
+	if algorithm == crypto.SHA1 {
+		data = EncryptSHA(raw)
+	} else {
+		data = EncryptMD5(EncryptSHA(raw))
+	}
+	signed, err := rsa.SignPKCS1v15(rand.Reader, priv, algorithm, data)
+	if err != nil {
+		return nil
+	}
+	return signed
+}
+
+//VerifySignature verify whether the given signature is correct
+func VerifySignature(raw []byte, signature string, algorithm crypto.Hash, publicKey string) bool {
+	if raw == nil || signature == "" {
+		return false
+	}
+	publicKey := []byte(publicKey)
+
+	block, _ := pem.Decode(publicKey)
+	if block == nil {
+		return false
+	}
+	pubInterface, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return false
+	}
+	pub := pubInterface.(*rsa.PublicKey)
+	var data []byte
+	if algorithm == crypto.SHA1 {
+		data = EncryptSHA(raw)
+	} else {
+		data = EncryptMD5(EncryptSHA(raw))
+	}
+	err = rsa.VerifyPKCS1v15(pub, algorithm, data, DecryptBase64(signature))
+	if err != nil {
+		return false
+	}
+	return true
+}
